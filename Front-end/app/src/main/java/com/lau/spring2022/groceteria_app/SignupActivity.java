3fb01122564da7d0588ee774d5f69bf3c6f11069
Omar Malaeb <omarmlb002@gmail.com>
2022-04-26 00:20:41 +0300

    designed signup page

--- Front-end/app/src/main/java/com/lau/spring2022/groceteria_app/SignupActivity.java
+++ Front-end/app/src/main/java/com/lau/spring2022/groceteria_app/SignupActivity.java
@@ -2,13 +2,35 @@ package com.lau.spring2022.groceteria_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SignupActivity extends AppCompatActivity {
+
+    //Page 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide the title bar of this activity screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_signup);
+    }
+
+    public void signup(View view) {
+
+    }
+
+    // when the TextView (Not a member yet? Sign up here.) is clicked, the signup page will appear
+    public void goLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
